346cec20980f1eae332fa374ff8d77dff0d641cf
claudio-code <suporte2424@outlook.com>
2022-04-10 15:56:26 -0300

    added use case to create client

---
+++ src/Application/Client/CreateClientUseCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Application\Client;
+
+use App\Domain\Entity\Client as ClientDomain;
+use App\Infrastructure\Persistence\Entity\Client;
+use App\Infrastructure\Persistence\Repository\ClientRepository;
+
+class CreateClientUseCase
+{
+    public function __construct(private ClientRepository $clientRepository)
+    {
+    }
+
+    public function create(CreateClientInputData $inputData): Client
+    {
+        $domain = $this->toDocument($inputData);
+        return $this->clientRepository->create($domain);
+    }
+
+    private function toDocument(CreateClientInputData $inputData): ClientDomain
+    {
+        return new ClientDomain(
+            name: $inputData->getName(),
+            email: $inputData->getEmail(),
+        );
+    }
+}
