2d42ed8257e4d084e7043b5591f8dd08feeb75a1
claudio-code <suporte2424@outlook.com>
2022-04-10 15:56:41 -0300

    create endpoint to create client

--- src/Api/Client/ClientEndPoint.php
+++ src/Api/Client/ClientEndPoint.php
@@ -2,6 +2,9 @@
 
 namespace App\Api\Client;
 
+use App\Application\Client\CreateClientInputData;
+use App\Application\Client\CreateClientUseCase;
+use App\Infrastructure\Form\ClientForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,5 +16,14 @@ class ClientEndPoint extends AbstractController
     public function findAll(): JsonResponse
     {
         return $this->json('client ok');
+    }
+
+    #[Route(path: "/client", methods: Request::METHOD_POST)]
+    public function create(Request $request, CreateClientUseCase $useCase): JsonResponse
+    {
+        $input = CreateClientInputData::makeEmpty();
+        $form = $this->createForm(ClientForm::class, $input);
+        $form->submit($request->request->all());
+        return $this->json($useCase->create($input));
     }
 }
