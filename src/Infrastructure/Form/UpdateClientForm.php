6d06803e4b1278a7c693b91e4fb1b26735cd28ef
claudio-code <suporte2424@outlook.com>
2022-04-10 21:06:10 -0300

    added form to update client

---
+++ src/Infrastructure/Form/UpdateClientForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Infrastructure\Form;
+
+use App\Application\Client\Create\CreateClientInputData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UpdateClientForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('id')
+            ->add('name')
+            ->add('email');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CreateClientInputData::class,
+            'csrf_protection' => false,
+        ]);
+    }
+}
