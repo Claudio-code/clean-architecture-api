78cee27425a6cc6820817931c8119c1a60d1078c
claudio-code <suporte2424@outlook.com>
2022-04-10 22:34:40 -0300

    added use case to remove client

---
+++ src/Application/Client/Remove/RemoveClientUseCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Application\Client\Remove;
+
+use App\Domain\Entity\ClientPathAndRemove;
+use App\Infrastructure\Persistence\Repository\ClientRepository;
+
+class RemoveClientUseCase
+{
+    public function __construct(private ClientRepository $clientRepository)
+    {
+    }
+
+    public function remove(RemoveClientInputData $inputData): void
+    {
+        $domain = $this->toDomain($inputData);
+        $this->clientRepository->delete($domain);
+    }
+
+    public function toDomain(RemoveClientInputData $inputData): ClientPathAndRemove
+    {
+        return new ClientPathAndRemove(email: $inputData->getEmail());
+    }
+}
