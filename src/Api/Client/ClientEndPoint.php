c5c952fbecc49ef739cedeb26581107af7f9464e
claudio-code <suporte2424@outlook.com>
2022-04-10 22:35:24 -0300

    added route to remove client

--- src/Api/Client/ClientEndPoint.php
+++ src/Api/Client/ClientEndPoint.php
@@ -6,6 +6,8 @@ use App\Application\Client\Create\CreateClientInputData;
 use App\Application\Client\Create\CreateClientUseCase;
 use App\Application\Client\FindAll\FindAllClientInputData;
 use App\Application\Client\FindAll\FindAllClientUseCase;
+use App\Application\Client\Remove\RemoveClientInputData;
+use App\Application\Client\Remove\RemoveClientUseCase;
 use App\Application\Client\Update\UpdateClientInputData;
 use App\Application\Client\Update\UpdateClientUseCase;
 use App\Infrastructure\Form\CreateClientForm;
@@ -13,11 +15,14 @@ use App\Infrastructure\Form\UpdateClientForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClientEndPoint extends AbstractController
 {
-    #[Route(path: "/client", methods: Request::METHOD_GET)]
+    private const ROUTE_PATH = '/client';
+
+    #[Route(path: self::ROUTE_PATH, methods: Request::METHOD_GET)]
     public function findAll(Request $request, FindAllClientUseCase $useCase): JsonResponse
     {
         $input = new FindAllClientInputData(
@@ -27,7 +32,7 @@ class ClientEndPoint extends AbstractController
         return $this->json($useCase->findAll($input));
     }
 
-    #[Route(path: "/client", methods: Request::METHOD_POST)]
+    #[Route(path: self::ROUTE_PATH, methods: Request::METHOD_POST)]
     public function create(Request $request, CreateClientUseCase $useCase): JsonResponse
     {
         $input = CreateClientInputData::makeEmpty();
@@ -36,12 +41,20 @@ class ClientEndPoint extends AbstractController
         return $this->json($useCase->create($input));
     }
 
-    #[Route(path: "/client", methods: Request::METHOD_PUT)]
+    #[Route(path: self::ROUTE_PATH, methods: Request::METHOD_PUT)]
     public function update(Request $request, UpdateClientUseCase $useCase): JsonResponse
     {
         $input = UpdateClientInputData::makeEmpty();
         $form = $this->createForm(UpdateClientForm::class, $input);
         $form->submit($request->request->all());
         return $this->json($useCase->update($input));
+    }
+
+    #[Route(path: self::ROUTE_PATH . "/{email}", methods: Request::METHOD_DELETE)]
+    public function remove(string $email, RemoveClientUseCase $useCase): JsonResponse
+    {
+        $input = new RemoveClientInputData($email);
+        $useCase->remove($input);
+        return $this->json([], Response::HTTP_NO_CONTENT);
     }
 }
