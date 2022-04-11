07bbc44c5ea595f621864a4deacb7a590ce7c986
claudio-code <suporte2424@outlook.com>
2022-04-10 22:34:03 -0300

    added domain to update fields and remove

---
+++ src/Domain/Entity/ClientPathAndRemove.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Domain\Entity;
+
+use App\Domain\Common\Email;
+use App\Domain\Common\StringValue;
+
+class ClientPathAndRemove
+{
+    private ?StringValue $id;
+    private ?StringValue $name;
+    private ?Email $email;
+
+    public function __construct(?string $id = null, ?string $name = null, ?string $email = null)
+    {
+        $this->setId($id);
+        $this->setName($name);
+        $this->setEmail($email);
+    }
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    private function setId(?string $id): void
+    {
+        $this->id =  match (is_string($id)) {
+            true => new StringValue($id),
+            default => $id,
+        };
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    private function setName(?string $name): void
+    {
+        $this->name =  match (is_string($name)) {
+            true => new StringValue($name),
+            default => $name,
+        };
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    private function setEmail(?string $email): void
+    {
+        $this->email = match (is_string($email)) {
+            true => new Email($email),
+            default => $email,
+        };
+    }
+}
