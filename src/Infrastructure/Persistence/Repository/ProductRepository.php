ebcadf2c433f0fbc62d80b2b5e7f19608e4c68f9
claudio-code <suporte2424@outlook.com>
2022-04-17 20:24:46 -0300

    added method to save product in database

--- src/Infrastructure/Persistence/Repository/ProductRepository.php
+++ src/Infrastructure/Persistence/Repository/ProductRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Infrastructure\Persistence\Repository;
 
+use App\Domain\Entity\Product as ProductDomain;
 use App\Infrastructure\Persistence\Entity\Product;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -11,5 +12,16 @@ class ProductRepository extends AbstractRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    public function create(ProductDomain $productDomain): Product
+    {
+        $product = new Product();
+        $product->setTitle($productDomain->getTitle());
+        $product->setImage($productDomain->getImage());
+        $product->setBrand($productDomain->getBrand());
+        $product->setPrice($productDomain->getPrice());
+        $this->persistWithTransaction($product);
+        return $product;
     }
 }
